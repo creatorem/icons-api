25d818e407c0c4a0e331b010996d5c03838acd1c
Arnaud Pfundstein <arnaudpfundstein.pro@gmail.com>
2023-03-09 19:08:39 +0100

    Clean: remove the axios scraper

--- scraper.js
+++ scraper.js
@@ -1,70 +1,97 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-const baseUrl = "https://react-icons.github.io";
-const homeSlug = "/react-icons";
+const dir = './icons';
+
+const baseUrl = 'https://react-icons.github.io';
+const homeSlug = '/react-icons';
 const iconPageSlugs = [];
 
 const getUrl = (slug) => baseUrl + slug;
-
-const request = async (slug) =>
-  await axios.request({
-    method: "GET",
-    url: getUrl(slug),
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    },
-  });
-
-const waitMenuIsLoaded = async (page) => {
-  while (page("#__next").length === 0) {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-  }
-};
-const waitIconsAreLoaded = async (page) => {
-  while (page(".icons .icon svg").length === 0) {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-  }
-};
-
-const getPage = async () => {
-  const axiosResponse = await request(homeSlug);
-  const $ = cheerio.load(axiosResponse.data);
-  await waitMenuIsLoaded($);
-
-  for (const el of $("#__next ul li a")) {
-    const slug = $(el).attr("href");
-    if (slug === homeSlug) continue;
-
-    if (!/^\/react-icons\/icons\?name=[\S]+$/.test(slug)) {
-      throw new Error("Invalid slug: " + slug);
+const findSlug = (str) => {
+    const slug = str.replace(/^\/react-icons\/icons\?name=([\S]+)$/g, '$1');
+    if (!slug) {
+        throw new Error('Invalid slug: ' + slug);
     }
-
-    iconPageSlugs.push(slug);
-  }
+    return slug;
 };
 
-const scrapIcons = async (slug) => {
-  console.log(slug);
-  const axiosResponse = await request(slug);
-  console.log( axiosResponse.data )
-  const $ = cheerio.load(axiosResponse.data);
-  await waitMenuIsLoaded($);
-  const icons = [];
+const queryPageElements = async (pageSlug, selector, callback, waitFor = null) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(getUrl(pageSlug));
+        await page.waitForSelector(waitFor ?? selector, { timeout: 1000 });
 
-  console.log($(".icons .icon svg"));
-  console.log("length here");
-  console.log($(".icons .icon svg").length);
-  for (const el of $(".icons .icon svg")) {
-    icons.push(el.outerHTML);
-  }
-  console.log(icons);
+        const elements = await page.$$(selector);
+
+        for (const el of Array.from(elements)) {
+            await callback(el, page);
+        }
+
+        await browser.close();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const createFileStructure = (slugs) => {
+    console.log(slugs);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    for (const slug of slugs) {
+        if (!fs.existsSync(dir + '/' + slug)) {
+            fs.mkdirSync(dir + '/' + slug);
+        }
+    }
 };
 
 const main = async () => {
-  await getPage();
-  await scrapIcons(iconPageSlugs[0]);
+    console.time('query icons');
+    await queryPageElements(homeSlug, '#__next ul li a', async (el) => {
+        const hrefHandle = await el.getProperty('href');
+        const slug = (await hrefHandle.jsonValue()).replace(baseUrl, '');
+        if (slug === homeSlug) return;
+
+        if (!/^\/react-icons\/icons\?name=[\S]+$/.test(slug)) {
+            throw new Error('Invalid slug: ' + slug);
+        }
+
+        iconPageSlugs.push(slug);
+    });
+
+    createFileStructure(iconPageSlugs.map((slug) => findSlug(slug)));
+
+    let i = 0;
+    for (const slugUrl of iconPageSlugs) {
+        console.log(++i + ' / ' + iconPageSlugs.length);
+        const slug = findSlug(slugUrl);
+
+        await queryPageElements(
+            slugUrl,
+            'div.icons > .item',
+            async (el) => {
+                let iconEl = await el.$('svg');
+                let i = 0;
+                while (!(iconEl = await el.$('svg')) && i < 10) {
+                    console.log('svg does not exist for slug ' + slug);
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
+                    i++;
+                }
+                const icon = await iconEl.evaluate((element) => element.outerHTML);
+                const nameEl = await el.$('.name');
+                const name = await nameEl.evaluate((element) => element.innerText);
+
+                fs.appendFile(`icons/${slug}/${name}.svg`, icon, function (err) {
+                    if (err) throw err;
+                });
+            },
+            'div.icons > .item svg'
+        );
+    }
+
+    console.timeEnd('query icons');
 };
 
 main();
